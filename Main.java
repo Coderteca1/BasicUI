a8a8e809c624fd4a5a1366d49a63c625eb8498c1
ZonaProgramable <59952622+ZonaProgramable@users.noreply.github.com>
2020-01-16 02:24:28 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+/**
+ * @author Zona programable
+ *
+ *         Clase principal.
+ */
+public class Main {
+
+	/**
+	 * Metodo main (Clasico)
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		View view = new View();// Crea el objeto view
+		view.setSize(800, 600);// Dimensiones de la ventana
+		view.setLocationRelativeTo(null);// centra la ventana en la pantalla
+		view.setVisible(true);// muestra la venata.
+
+	}
+
+}
